1895d932dd8e09d3f2ecf5dd205241a78b346c89
georgelamb19 <wglamb196@gmail.com>
2020-06-29 13:07:25 +0100

    notebook imports

--- chemprop/__init__.py
+++ chemprop/__init__.py
@@ -2,7 +2,7 @@ import chemprop.data
 import chemprop.features
 import chemprop.models
 import chemprop.train
-
+import chemprop.trainBayes
 import chemprop.args
 import chemprop.nn_utils
 import chemprop.utils
